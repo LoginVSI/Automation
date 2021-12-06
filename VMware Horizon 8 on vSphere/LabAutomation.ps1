443a0ae7057d46476ce4776fa8f7fe702ce83439
mplette <77774262+mplette@users.noreply.github.com>
2021-12-06 15:47:54 +0100

    Merge ESXautomation into file

--- VMware Horizon 8 on vSphere/LabAutomation.ps1
+++ VMware Horizon 8 on vSphere/LabAutomation.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host Login Enterprise - Example Automation
+﻿write-Host Login Enterprise - Example Automation
 #Read configuration file
 If ($config -eq $null)
 {
@@ -11,7 +11,6 @@ Write-host Starting test: $Testname
 
 
 #Create a directory to create a local copy of ESXtop logfiles
-
 New-Item -ItemType "directory" -Path $config.Automation_LogFilesPathWhenCompleted -ErrorAction SilentlyContinue
 $TranscriptLocation = $config.Automation_LogFilesPathWhenCompleted+"\"+$Testname+".log"
 write-host Storing local logfiles at: $TranscriptLocation
@@ -23,7 +22,6 @@ $MaxTestPreparationTime=(get-date).AddMinutes($config.LoginEnterprise_MaxTestPre
 
 #Load Login enterprise stuff
  . $PSScriptRoot\LoginEnterpriseFunctions.ps1
- . $PSScriptRoot\ESXiAutomation.ps1
  
 # Loading powercli modules
 Import-Module VMware.VimAutomation.HorizonView
@@ -52,7 +50,7 @@ write-host $(Get-Date) (Get-LeLaunchers).count "Launchers reported ready and onl
 
 #Reboot all VMs in desktop pool
 $VMS = get-vm $config.VMwareHorizon_PoolVMNamingPattern
-    write-host $(Get-Date) "Reboot all machines in desktop-pool:" $VMS.Count
+    write-host $(Get-Date) "Reboot all machines in desktop-pool" $config.VMwareHorizon_PoolName":" $VMS.Count
     foreach ($vm in $VMS){
         try {
         $stopVM=Stop-VM -VM $VM.name -Confirm:$False -RunAsync
@@ -82,13 +80,76 @@ If ($MaxTestPreparationTimeLeft.Minutes -gt 0)
 
 
 #Start capturing performance data (ESXtop) 
-Capture-HostData1 -HostName $config.VMwareESXi_Hosts -TestName $Testname
+Capture-HostData -HostName $config.VMwareESXi_Hosts -TestName $Testname
 
 
 #Connect to Login Enterprise, start test and wait for completion
 Start-Test($config.LoginEnterprise_TestID)
 Wait-Test($config.LoginEnterprise_TestID)
 
-Collect-HostData -Hostname $config.VMwareESXi_Hosts -Testname $Testname -LogFilePathWhenCompleted $config.Automation_LogFilesPathWhenCompleted"\"
+Collect-HostDataESX -Hostname $config.VMwareESXi_Hosts -Testname $Testname -LogFilePathWhenCompleted $config.Automation_LogFilesPathWhenCompleted"\"
+
+function Capture-HostData {
+  Param(  
+        [string]$HostName,
+        [string]$TestName)
+
+    Write-Host "$(Get-Date)  Starting performance data capture on hypervisor:" $hostname
 
 
+    $delay = 30
+    $Duration = 60
+    $timeout = [math]::Round($Duration + ($delay * 5))
+    $path = $LogFilePath + $TestName + "-" + $HostName + ".csv"
+    $path
+  
+
+    $command = "/bin/esxtop -b -d $delay -n 96 > $path&"
+    $command 
+    
+    $ESXpassword = ConvertTo-SecureString $config.VMwareESXi_Password -AsPlainText -Force
+    $hostCredential = New-Object System.Management.Automation.PSCredential $config.VMwareESXi_UserName, $ESXpassword
+    $session = New-SSHSession -ComputerName $HostName -Credential $hostCredential -Force
+    
+    try {
+    Invoke-SSHCommand -Index $session.SessionId -Command $command -TimeOut 5
+    }catch {
+    }
+    
+    
+    Get-SSHSession | Remove-SSHSession | Out-Null
+    }
+
+Function Collect-HostDataESX {
+    Param(
+        [string]$HostName,
+        [string]$TestName,
+        [string]$LogFilePathWhenCompleted
+    )
+    Write-Host "$(Get-Date)  Collect  performance data from hypervisor." $HostName
+    Write-Host "$(Get-Date)  Testname " $TestName
+    Write-Host "$(Get-Date)  LogFilePathWhenCompleted " $LogFilePathWhenCompleted
+    
+   $ESXpassword = ConvertTo-SecureString $config.VMwareESXi_Password -AsPlainText -Force
+   $hostCredential = New-Object System.Management.Automation.PSCredential $config.VMwareESXi_UserName, $ESXpassword
+
+   $remoteFile = $LogFilePath + $TestName + "-" + $HostName + ".csv"
+   $localFile = $config.Automation_LogFilesPathWhenCompleted + "\" + $TestName + "-" + $HostName + ".csv"
+        
+     	try
+		{
+			Get-SCPFile -HostName $HostName -RemoteFile $remoteFile -LocalFile $localFile -Credential $hostCredential -Force -OperationTimeout 300 -ConnectionTimeOut 300	
+			Write-Host "$(Get-Date)  Cleaning up old perf counters" 
+			$session = New-SSHSession -ComputerName $HostName -Credential $hostCredential -Force -ConnectionTimeOut 60
+			Invoke-SSHCommand -Index $session.SessionId -Command "rm -rf $remoteFile" -TimeOut 60 
+			Get-SSHSession | Remove-SSHSession | Out-Null
+		}
+		catch
+		{
+			Write-Host "$(Get-Date)  Could not collect performance information from $hostname : $_"
+		
+		}
+		
+        
+        
+    }
